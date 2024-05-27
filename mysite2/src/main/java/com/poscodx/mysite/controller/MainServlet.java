c9954e93ad6950d8b41cdea65e03d1c1fc1c6765
seojeongHHHui <sjhui0523@gmail.com>
2024-05-27 13:01:27 +0900

    refactoring #1

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -1,21 +1,12 @@
 package com.poscodx.mysite.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.poscodx.mysite.controller.action.main.MainAction;
 
-public class MainServlet extends HttpServlet {
+public class MainServlet extends ActionServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request
-		.getRequestDispatcher("/WEB-INF/views/main/index.jsp")
-		.forward(request, response);	
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	@Override
+	protected Action getAction(String actionName) {
+		return new MainAction();
 	}
 }
--- mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
@@ -10,48 +10,53 @@ import javax.servlet.http.HttpServletResponse;
 import com.poscodx.mysite.dao.UserDao;
 import com.poscodx.mysite.vo.UserVo;
 
-public class UserServlet extends HttpServlet {
+public class UserServlet extends ActionServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		
-		String action = request.getParameter("a");
-		if("joinform".equals(action)) {
-			request
-			.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
-			.forward(request, response);
-			
-		} else if("join".equals(action)) {
-			String name = request.getParameter("name");
-			String email = request.getParameter("email");
-			String password = request.getParameter("password");
-			String gender = request.getParameter("gender");
-			
-			UserVo vo = new UserVo();
-			vo.setName(name);
-			vo.setEmail(email);
-			vo.setPassword(password);
-			vo.setGender(gender);
-			
-			// System.out.println(vo); //test
-			new UserDao().insert(vo);
-			
-			response.sendRedirect(request.getContextPath()+"/user?a=joinsuccess");
-			
-		} else if("joinsuccess".equals(action)) {
-			request
-			.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
-			.forward(request, response);
-			
-		} else {
-			response.sendRedirect(request.getContextPath());
-		}
-		
+	@Override
+	protected Action getAction(String actionName) {
+		return null;
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		request.setCharacterEncoding("utf-8");
+//		
+//		String action = request.getParameter("a");
+//		if("joinform".equals(action)) {
+//			request
+//			.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+//			.forward(request, response);
+//			
+//		} else if("join".equals(action)) {
+//			String name = request.getParameter("name");
+//			String email = request.getParameter("email");
+//			String password = request.getParameter("password");
+//			String gender = request.getParameter("gender");
+//			
+//			UserVo vo = new UserVo();
+//			vo.setName(name);
+//			vo.setEmail(email);
+//			vo.setPassword(password);
+//			vo.setGender(gender);
+//			
+//			// System.out.println(vo); //test
+//			new UserDao().insert(vo);
+//			
+//			response.sendRedirect(request.getContextPath()+"/user?a=joinsuccess");
+//			
+//		} else if("joinsuccess".equals(action)) {
+//			request
+//			.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
+//			.forward(request, response);
+//			
+//		} else {
+//			response.sendRedirect(request.getContextPath());
+//		}
+//		
+//	}
+//
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+//	}
 
 }

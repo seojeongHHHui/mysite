53f96b91da96c2165448dbb487967b40d50387b9
seojeongHHHui <sjhui0523@gmail.com>
2024-06-07 16:35:01 +0900

    tx

--- mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
@@ -1,12 +1,12 @@
 package com.poscodx.mysite.service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.poscodx.mysite.repository.GuestbookLogRepository;
 import com.poscodx.mysite.repository.GuestbookRepository;
 import com.poscodx.mysite.vo.GuestbookVo;
 
@@ -16,23 +16,26 @@ public class GuestbookService {
 	@Autowired
 	private GuestbookRepository guestbookRepository;
 	
+	@Autowired
+	private GuestbookLogRepository guestbookLogRepository;
+	
 	public List<GuestbookVo> getContentsList() {
 		return guestbookRepository.findAll();
 	}
 	
+	@Transactional
 	public void deleteContents(Long no, String password) {
+		guestbookLogRepository.update(no);
 		guestbookRepository.deleteByNoAndPassword(no, password);
 	}
 	
+	@Transactional
 	public void addContents(GuestbookVo vo) {
-//		Date now = new Date();
-//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//		String regDate = dateFormat.format(now);
-//		vo.setRegDate(regDate);
-		
-		System.out.println(vo);
+		int count = guestbookLogRepository.update();
+		if(count == 0) {
+			guestbookLogRepository.insert();
+		}
 		guestbookRepository.insert(vo);
-		System.out.println(vo);
 	}
 	
 }

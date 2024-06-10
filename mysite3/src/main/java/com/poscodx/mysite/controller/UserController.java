3929066651b66ed6c2e583ad89d85c5a57e7a09e
seojeongHHHui <sjhui0523@gmail.com>
2024-06-10 15:43:16 +0900

    security: Login&logout Interceptor

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -38,29 +38,6 @@ public class UserController {
 		return "user/login";
 	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login(HttpSession session, UserVo vo, Model model) {
-		UserVo authUser = userService.getUser(vo.getEmail(), vo.getPassword());
-		if(authUser == null) {
-			model.addAttribute("email", vo.getEmail());
-			model.addAttribute("result", "fail");
-			
-			return "user/login";
-		}
-		
-		// login 처리
-		session.setAttribute("authUser", authUser);
-		
-		return "redirect:/";
-	}
-	
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
-	}
-	
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
 		// access control

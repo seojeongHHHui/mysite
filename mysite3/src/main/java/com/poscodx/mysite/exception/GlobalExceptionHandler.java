631c36ebb305db17c6247d579029cd54f04e90c5
seojeongHHHui <sjhui0523@gmail.com>
2024-06-12 13:32:38 +0900

    logging 적용

--- mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -3,19 +3,22 @@ package com.poscodx.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
 	public String handler(Exception e, Model model) {
 		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
+		logger.error(errors.toString());
 		
 		// 2. 사과(종료)
 		model.addAttribute("error", errors.toString());

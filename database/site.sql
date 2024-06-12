43699fb7d5e583f49107010d6c5a12f71643e8fa
seojeongHHHui <sjhui0523@gmail.com>
2024-06-12 10:42:15 +0900

    site.sql

--- database/site.sql
+++ database/site.sql
@@ -2,3 +2,7 @@
 desc site;
 insert into site values(null, 'MySite', '안녕하세요. 서정희의 mysite에 오신 것을 환영합니다.', '/assets/images/bearggu.png', '이 사이트는 웹 프로그램밍 실습과제 예제 사이트입니다. 메뉴는 사이트 소개, 방명록, 게시판이 있구요. 자바수업 + 데이터베이스 수업 + 웹프로그래밍 수업 배운 거 있는거 없는 거 다 합쳐서 만들어 놓은 사이트 입니다.');
 select * from site;
+
+update site
+set title=#{title }, welcome=#{welcome }, profile=#{profile }, description=#{description }
+where no=#{no };

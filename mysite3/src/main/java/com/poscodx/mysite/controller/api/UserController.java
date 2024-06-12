5dcc9e756ec3b0bc31442aae79dec5959334d84a
seojeongHHHui <sjhui0523@gmail.com>
2024-06-12 17:08:04 +0900

    check email #finish

--- mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
@@ -3,23 +3,23 @@ package com.poscodx.mysite.controller.api;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
 
-@Controller("userApiController")
+@RestController("userApiController")
 @RequestMapping("/user/api")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	@ResponseBody
-	@RequestMapping("/checkemail")
+	@GetMapping("/checkemail")
 	public Object checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
 		
 		UserVo vo = userService.getUser(email);

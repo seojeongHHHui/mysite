1894f01829d5e0de91740abd09d7cade2c98ff68
seojeongHHHui <sjhui0523@gmail.com>
2024-06-24 13:55:26 +0900

    ResultHandler

--- mysite5/src/main/java/com/poscodx/mysite/repository/UserRepository.java
+++ mysite5/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.poscodx.mysite.repository;
 
 import java.util.Map;
 
+import org.apache.ibatis.session.ResultContext;
+import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
-import com.poscodx.mysite.security.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poscodx.mysite.vo.UserVo;
 
 @Repository
@@ -29,17 +31,26 @@ public class UserRepository {
 		return sqlSession.selectOne("user.findByNo", no);
 	}
 	
-//	public UserVo findByEmail(String email) {
-//		return sqlSession.selectOne("user.findByEmail", email);
-//	}
-//	
-//	public UserDetailsImpl findByEmail2(String email) {
-//		return sqlSession.selectOne("user.findByEmail2", email);
-//	}
-	
-	public <R> R findByEmail(String email) {
-		R result = null;
-		return result;
+	public <R> R findByEmail(String email, Class<R> resultType) {
+		FindByEmailResultHandler<R> findByEmailResultHandler = new FindByEmailResultHandler<>(resultType);
+		sqlSession.select("user.findByEmail", email, findByEmailResultHandler);
+
+		return findByEmailResultHandler.result;
+	}
+
+	private class FindByEmailResultHandler<R> implements ResultHandler<Map<String, Object>> {
+		private R result;
+		private Class<R> resultType;
+
+		FindByEmailResultHandler(Class<R> resultType) {
+			this.resultType = resultType;
+		}
+
+		@Override
+		public void handleResult(ResultContext<? extends Map<String, Object>> resultContext) {
+			Map<String, Object> resultMap = resultContext.getResultObject();
+			result = new ObjectMapper().convertValue(resultMap, resultType);
+		}
 	}
 
 	public int update(UserVo vo) {
